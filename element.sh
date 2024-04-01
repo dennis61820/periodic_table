5ce667d57c57f93910c8d597cc19da76d6285174
dennis61820 <danngit@hotmail.com>
2024-04-01 15:57:48 +0000

    fix: fixed a boo boo

--- element.sh
+++ element.sh
@@ -3,10 +3,6 @@
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 
-
-
-
-
  if [[ -z $1 ]]
   then
     echo  "Please provide an element as an argument."
@@ -51,6 +47,7 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
       fi
 
 fi
+
 
 
 
